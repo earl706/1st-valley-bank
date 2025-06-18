d4c797c4d51da5509b40660da81b3d869e3ac335
kylo ren <oneearl706@gmail.com>
2025-06-18 16:04:35 +0800

    Implemented Salary Page

--- src/pages/LoansGoldAndGems.jsx
+++ src/pages/LoansGoldAndGems.jsx
@@ -1,5 +1,177 @@
+import {
+  CreditCardIcon,
+  DetectiveIcon,
+  MoneyWavyIcon,
+  PlusMinusIcon,
+  ProjectorScreenChartIcon,
+  ShareNetworkIcon,
+  SketchLogoIcon,
+  TrendDownIcon,
+  TrendUpIcon,
+} from "@phosphor-icons/react/dist/ssr";
+import img1 from "/src/assets/loans/gold-and-gems/1.jpg";
 import React from "react";
+import { ShieldCheckIcon } from "@phosphor-icons/react";
 
 export default function LoansGoldAndGems() {
-  return <div>Loans Gold And Gems</div>;
+  const advantages = [
+    {
+      logo: <TrendUpIcon size={100} />,
+      description: "High Appraisal",
+    },
+    {
+      logo: <TrendDownIcon size={100} />,
+      description: "Lowest Interest Rate",
+    },
+    {
+      logo: <ProjectorScreenChartIcon size={100} />,
+      description: "Longer Term",
+    },
+    {
+      logo: <ShareNetworkIcon size={100} />,
+      description: "Flexible Repayment Term",
+    },
+    {
+      logo: <PlusMinusIcon size={100} />,
+      description: "No Service Charge",
+    },
+    {
+      logo: <ShieldCheckIcon size={100} />,
+      description: "Safety",
+    },
+    {
+      logo: <DetectiveIcon size={100} />,
+      description: "Confidentiality",
+    },
+    {
+      logo: <CreditCardIcon size={100} />,
+      description: "Remote Payment",
+    },
+  ];
+
+  return (
+    <>
+      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+        <section
+          id="main"
+          data-scroll
+          className="flex text-[#396131] bg-white drop-shadow-lg rounded-[8px] py-[80px] px-[80px] mx-[10px]"
+        >
+          <div className="flex flex-col gap-[20px] w-3/5">
+            <span className="text-[4rem]/[4rem] font-bold">
+              Loans / Gold & Gems
+            </span>
+            <span className="text-[1rem]/[2rem]">
+              Secure quick cash with 1st Valley Bank’s Gold and Gems Loan—a
+              hassle-free lending solution using your valuable jewelry as
+              collateral. Enjoy low interest rates, fast approval, and flexible
+              terms. Perfect for urgent needs or personal expenses. Your
+              treasures stay safe while your finances stay strong.
+            </span>
+          </div>
+          <div className="flex justify-center w-2/5">
+            <SketchLogoIcon className="w-[50%] h-auto" />
+          </div>
+        </section>
+        <section id="description" className="px-[15px] text-white">
+          <div className="flex px-[80px] py-[60px] gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex justify-center w-2/5">
+              <MoneyWavyIcon className="w-[50%] h-auto" />
+            </div>
+            <div className="flex flex-col items-start justify-start gap-[50px] w-3/5">
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-white font-bold text-[3rem]/[3rem]">
+                  Gold & Gems
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  Individual borrowers may pledge their genuine pieces of
+                  jewelry for instant cash for personal purposes. This is
+                  through the Gold & Gems jewelry loan. Interest rates are most
+                  affordable, and the term is guaranteed to be flexible.
+                </span>
+              </div>
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-white font-bold text-[3rem]/[3rem]">
+                  Jewelry Loan
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  For quick cash, borrowers may find their emergency fix in
+                  1VB's jewelry business loan. All clients need are authentic
+                  jewelry items. Clients may exchange these items for cash that
+                  they can use to expand their businesses.
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          id="description-2"
+          className="flex justify-between mx-[60px] text-[#396131]"
+        >
+          <div className="flex flex-col gap-[50px] w-3/5">
+            <span className="text-[2.5rem]/[2.5rem] font-bold">
+              Small Business Loan Ends 2023 with Php385M
+            </span>
+            <span className="text-[2rem]/[2rem] font-semibold">
+              Ervie E. Cane, Product Manager
+            </span>
+            {[
+              {
+                header: "2023: Turnaround Year for Gold & Gems",
+                description:
+                  "After the 3-year drought for Gold and Gems, where portfolio decreased, the product has recovered successfully with a 40M increase in 2023, its turnaround year. The increase was a result of the following: higher appraisal rate, innovative features, and concerted effort of the Gold & Gems team. ",
+              },
+              {
+                header: "Why Choose 1st Valley Bank",
+                description:
+                  "Several good reasons. Foremost is that aside from the advantages, one is assured that the Bank wants the borrower to regain possession of the item/s submitted as pledge. ",
+              },
+              {
+                header: "Jewelry Business Loan",
+                description:
+                  "This loan grants business funds to anyone with authentic jewelry pieces to submit as collateral. It's the fastest way to get credit. Just convert your idle jewelry pieces. Let them shine and sparkle in times of need.",
+              },
+            ].map((part, index) => (
+              <div className="flex flex-col gap-[20px]" key={index}>
+                <span className="text-[1.5rem]/[2rem] font-semibold">
+                  {part.header}
+                </span>
+                <span className="text-[1rem]/[3rem]">{part.description}</span>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center w-2/5 h-full">
+            <img
+              src={img1}
+              alt=""
+              className="object-cover aspect-square rounded-[12px] w-4/5 h-full"
+            />
+          </div>
+        </section>
+        <section
+          id="who-can-apply"
+          className="flex flex-col p-[50px] gap-[50px] bg-[#396131] text-white mx-[15px] rounded-[10px] drop-shadow-lg"
+        >
+          <span className="text-center font-bold text-[2rem]/[2rem]">
+            Advantages
+          </span>
+          <div className="grid grid-cols-4 gap-x-[20px] gap-y-[50px]">
+            {advantages.map((loan, index) => (
+              <div
+                className="flex flex-col items-center gap-[10px]"
+                key={index}
+              >
+                <div className="flex">{loan.logo}</div>
+                <span className="text-[1rem]/[3rem] text-center">
+                  {loan.description}
+                </span>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+      <main className="hidden sm:block lg:hidden">Tablet</main>
+      <main className="block sm:hidden">Mobile</main>
+    </>
+  );
 }
--- src/pages/LoansSalary.jsx
+++ src/pages/LoansSalary.jsx
@@ -1,5 +1,200 @@
+import { faCommentsDollar } from "@fortawesome/free-solid-svg-icons/faCommentsDollar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import img1 from "/src/assets/loans/salary/1.png";
+import img2 from "/src/assets/loans/salary/2.jpg";
 import React from "react";
+import {
+  HandArrowDownIcon,
+  IdentificationCardIcon,
+} from "@phosphor-icons/react/dist/ssr";
 
 export default function LoansSalary() {
-  return <div>Loans Salary</div>;
+  return (
+    <>
+      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+        <section
+          id="main"
+          data-scroll
+          className="flex text-[#396131] bg-white drop-shadow-lg rounded-[8px] py-[80px] px-[80px] mx-[10px]"
+        >
+          <div className="flex flex-col gap-[20px] w-3/5">
+            <span className="text-[4rem]/[4rem] font-bold">Loans / Salary</span>
+            <span className="text-[1rem]/[2rem]">
+              Need extra funds before payday? 1st Valley Bank’s Salary Loan
+              offers fast, convenient, and affordable financing for employed
+              individuals. Enjoy low interest rates, quick approval, and
+              flexible payment terms. Whether it’s for bills, emergencies, or
+              personal needs—get the support you need, right when you need it.
+            </span>
+          </div>
+          <div className="flex justify-center w-2/5">
+            <FontAwesomeIcon
+              icon={faCommentsDollar}
+              style={{ width: "50%", height: "auto" }}
+            />
+          </div>
+        </section>
+        <section id="description" className="px-[15px] text-white">
+          <div className="flex px-[80px] py-[60px] gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex justify-center w-2/5">
+              <img
+                src={img1}
+                alt=""
+                className="object-cover rounded-[12px] w-[80%] h-auto"
+              />
+            </div>
+            <div className="flex flex-col items-start justify-start gap-[50px] w-3/5">
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-white font-bold text-[3rem]/[3rem]">
+                  BARANGAY LOANS
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  This loan is granted to elected barangay officials such as the
+                  chairman and kagawads and to appointed barangay staff such as
+                  the treasurer and secretary.
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  Barangay officials with monthly honorarium of more than Phpl5K
+                  may avail a loan amount of up to Php300K, while those with
+                  monthly honorarium of less than Phpl5K may avail an amount of
+                  up to Php25OK payable in 32 months. However, the figures are
+                  variable as these are dependent on the conditions of the
+                  election.
+                </span>
+              </div>
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-white font-bold text-[3rem]/[3rem]">
+                  LGU LOANS
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  Employees of local government units that have entered into a
+                  memorandum agreement with 1VBDB may apply for our LGU salary
+                  loan.
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  Qualified LGU employees may borrow up to Php700K for their
+                  repeat loan, and up to Php450K for their initial loan. This is
+                  payable within three (3) years, but the term may depend on
+                  other factors such as age and status of employment, i.e.
+                  permanent, casual, and if elected or co-terminus.
+                </span>
+                <span className="text-white text-[0.8rem]/[2.4rem]">
+                  Payment is collected monthly and though payroll deduction.
+                  This loan requires the borrowers to maintain a savings account
+                  with the bank.
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          id="types"
+          data-scroll
+          className="flex flex-col gap-[30px] text-[#396131] px-[60px] mx-[10px]"
+        >
+          <div className="flex">
+            <div className="flex flex-col gap-[20px] w-3/5">
+              <span className="text-[4rem]/[4rem] font-bold">APDS LOANS</span>
+              <span className="text-[1rem]/[2rem]">
+                Automatic Payroll Deduction is the type of salary loan available
+                to public school teachers with permanent appointment issued by
+                the Department of Education (DepEd). Qualified teachers may
+                borrow up to Php6O0K payable in three (3) years through monthly
+                amortization. The amount representing the monthly amortization
+                is deducted automatically from the payroll.
+              </span>
+            </div>
+            <div className="flex justify-center w-2/5">
+              <HandArrowDownIcon style={{ width: "50%", height: "auto" }} />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center w-2/5">
+              <IdentificationCardIcon
+                style={{ width: "50%", height: "auto" }}
+              />
+            </div>
+            <div className="flex flex-col gap-[20px] w-3/5">
+              <span className="text-[4rem]/[4rem] font-bold">
+                ATM + BONUS LOANS
+              </span>
+              <span className="text-[1rem]/[2rem]">
+                ATM plus Bonus loans are bundled salary loans available to
+                public school teachers with permanent appointment from DepEd. It
+                is similar to automatic payroll deduction salary (APDS) loan;
+                only, instead of the amortization being automatically deducted
+                in the payroll, teachers have to leave their ATM card and PIN in
+                the custody of 1VBDB.
+              </span>
+              <span className="text-[1rem]/[2rem]">
+                Teachers who have availed the ATM loan may also borrow
+                additional money through the Bonus Loan. Payment is charged
+                against the Bonus compensation of the teachers and effected
+                through their ATM. This is why teachers must have to qualify for
+                the ATM loan first before they can be eligible for the Bonus
+                loan.
+              </span>
+            </div>
+          </div>
+        </section>
+        <section id="product-manager" className="px-[15px] text-white">
+          <div className="flex px-[80px] py-[60px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex justify-center w-2/5">
+              <img
+                src={img2}
+                alt=""
+                className="object-cover rounded-[12px] w-[80%] h-auto"
+              />
+            </div>
+            <div className="flex flex-col items-start justify-start gap-[70px] w-3/5">
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-[3rem]/[3rem] font-bold">
+                  Salary Loans That Put a Smile on Your Face
+                </span>
+                <span className="text-[2rem]/[2rem] font-bold">
+                  Juba! Y. Yu, Product Manager{" "}
+                </span>
+              </div>
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-[1.5rem]/[1.5rem] font-bold">
+                  More than 1.5B Increase in Just 3 Months!
+                </span>
+                <span className="text-[1rem]/[3rem]">
+                  Salary loans beefed up 1VB's loan performance with a whopping
+                  Php1.5B increase in barely three (3) months, a feat that has
+                  never been done in the past ten (10) years.
+                </span>
+              </div>
+              <span className="text-[1rem]/[3rem]">
+                This loan provides Dep-Ed teachers and eligible personnel with
+                the funds they need to augment their salaries. Dep-Ed loans are
+                under the APDS (automatic payroll deduction scheme) to make it
+                convenient for borrowers to pay their dues without having to
+                leave their classrooms or offices.
+              </span>
+              <span className="text-[1rem]/[3rem]">
+                Convenient and accessible, the LGU and Barangay Loans serve the
+                financial needs of elected or appointed officials, as well as
+                regular staff of local government units (LGUs) and barangays.
+                Payments are made through automatic payroll deduction.
+              </span>
+              <div className="flex flex-col gap-[20px]">
+                <span className="text-[1.5rem]/[1.5rem] font-bold">
+                  ATM & Bonus Loans
+                </span>
+                <span className="text-[1rem]/[3rem]">
+                  Through this loan product, regular employees of Dep-ed, local
+                  government units and private institutions may borrow funds
+                  from the bank. Repayment is chargeable against their automated
+                  teller machine (ATM) payroll account.
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <main className="hidden sm:block lg:hidden">Tablet</main>
+      <main className="block sm:hidden">Mobile</main>
+    </>
+  );
 }
