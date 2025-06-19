cdc18be196ffdf8f1ed48eb3da464f38a744d201
kylo ren <oneearl706@gmail.com>
2025-06-19 21:37:41 +0800

    Implemented responsive Loans Page

--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -68,15 +68,17 @@ export default function Loans() {
 
   return (
     <>
-      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+      <main className="flex flex-col gap-[80px] pb-[50px]">
         <section
           id="main"
           data-scroll
-          className="flex text-[#396131] bg-white drop-shadow-lg mx-[10px] rounded-[8px] py-[50px]"
+          className="flex flex-col-reverse lg:flex-row text-[#396131] gap-[20px] lg:gap-auto bg-white drop-shadow-lg mx-[5px] rounded-[8px] py-[20px] lg:py-[50px]"
         >
-          <div className="flex flex-col gap-[20px] py-[80px] px-[60px] mx-[10px] w-2/3">
-            <span className="text-[4rem]/[4rem] font-bold">Loans</span>
-            <span className="text-[1rem]/[2rem]">
+          <div className="flex flex-col gap-[20px] p-[20px] lg:py-[80px] lg:px-[60px] mx-[10px] lg:w-2/3">
+            <span className="text-[2rem]/[2rem] lg:text-[4rem]/[4rem] font-bold">
+              Loans
+            </span>
+            <span className="text-[0.8rem]/[2.4rem] lg:text-[1rem]/[2rem]">
               Fuel your dreams with 1st Valley Bank Loans! Whether you’re
               growing a business, investing in agriculture, or managing daily
               needs, our flexible loan options come with competitive rates and
@@ -85,7 +87,7 @@ export default function Loans() {
               within reach—let’s make them happen!
             </span>
           </div>
-          <div className="flex items-center justify-center w-1/3">
+          <div className="flex items-center justify-center lg:w-1/3">
             <FontAwesomeIcon
               icon={faBank}
               className="flex aspect-square"
@@ -169,8 +171,6 @@ export default function Loans() {
           </div>
         </section>
       </main>
-      <main className="hidden sm:block lg:hidden">Tablet</main>
-      <main className="block sm:hidden">Mobile</main>
     </>
   );
 }
