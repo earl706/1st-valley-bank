c7432022fdcf20b60e4fe9f747fa73cbf76ab1b8
kylo ren <oneearl706@gmail.com>
2025-06-20 10:05:24 +0800

    Implemented responsive Microfinance page

--- src/pages/LoansMicrofinance.jsx
+++ src/pages/LoansMicrofinance.jsx
@@ -7,40 +7,40 @@ import React from "react";
 export default function LoansMicrofinance() {
   return (
     <>
-      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+      <main className="flex flex-col gap-[80px] pb-[50px]">
         <section
           id="main"
           data-scroll
-          className="flex text-[#396131] bg-white drop-shadow-lg rounded-[8px] py-[80px] px-[80px] mx-[10px]"
+          className="flex flex-col-reverse lg:flex-row text-[#396131] bg-white drop-shadow-lg gap-[20px] rounded-[8px] p-[30px] lg:py-[80px] lg:px-[80px] mx-[10px]"
         >
-          <div className="flex flex-col gap-[20px] w-3/5">
-            <span className="text-[4rem]/[4rem] font-bold">
+          <div className="flex flex-col gap-[20px] lg:w-3/5">
+            <span className="text-[2rem]/[2rem] lg:text-[4rem]/[4rem] font-bold">
               Loans / Microfinance
             </span>
-            <span className="text-[1rem]/[2rem]">
+            <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[2rem]">
               Credit program that provides affordable funds to
               micro-entrepreneurs or low-income individuals who wish to start or
               expand their business but have limited or no access to financial
               services.
             </span>
           </div>
-          <div className="flex items-center justify-center w-2/5">
+          <div className="flex items-center justify-center lg:w-2/5">
             <ChartPieSliceIcon className="w-[60%] h-auto" />
           </div>
         </section>
         <section id="description" className="px-[15px] text-white">
-          <div className="flex px-[80px] py-[60px] gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
-            <div className="flex items-center justify-center w-2/5">
+          <div className="flex flex-col lg:flex-row lg:px-[80px] p-[30px] lg:py-[60px] gap-[20px] lg:gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex items-center justify-center lg:w-2/5">
               <ChartBarHorizontalIcon className="w-[80%] h-auto" />
             </div>
-            <div className="flex flex-col items-start justify-start gap-[20px] w-3/5">
-              <span className="text-white font-bold text-[3rem]/[3rem]">
+            <div className="flex flex-col items-start justify-start gap-[10px] lg:gap-[20px] lg:w-3/5">
+              <span className="text-white font-bold text-[1.5rem]/[1.5rem] lg:text-[3rem]/[3rem]">
                 Small Business Loans
               </span>
-              <span className="text-white font-bold text-[2rem]/[2rem]">
+              <span className="text-white font-bold text-[1rem]/[1.5rem] lg:text-[2rem]/[2rem]">
                 Increase your income with affordable funds
               </span>
-              <span className="text-white text-[0.8rem]/[2.4rem]">
+              <span className="text-white text-[0.8rem]/[2.4rem] lg:text-[1rem]/[2.25rem]">
                 Micro-entrepreneurs including the micro-entrepreneurial poor
                 will benefit from this loan facility. This loan product allows
                 them to have access to affordable funds they can use to expand
@@ -66,8 +66,6 @@ export default function LoansMicrofinance() {
           </div>
         </section>
       </main>
-      <main className="hidden sm:block lg:hidden">Tablet</main>
-      <main className="block sm:hidden">Mobile</main>
     </>
   );
 }
