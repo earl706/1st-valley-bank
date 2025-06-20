e0a603278047f18344a3426f0ceea57e98d1fe5c
kylo ren <oneearl706@gmail.com>
2025-06-20 09:57:09 +0800

    Implemented SME Page

--- src/pages/LoansAgriculture.jsx
+++ src/pages/LoansAgriculture.jsx
@@ -11,17 +11,17 @@ import { ShieldCheckIcon, ShovelIcon } from "@phosphor-icons/react/dist/ssr";
 export default function LoansAgriculture() {
   return (
     <>
-      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+      <main className="flex flex-col gap-[40px] lg:gap-[80px] pb-[50px]">
         <section
           id="main"
           data-scroll
-          className="flex text-[#396131] bg-white drop-shadow-lg mx-[10px] rounded-[8px] py-[50px]"
+          className="flex flex-col-reverse lg:flex-row text-[#396131] bg-white drop-shadow-lg mx-[5px] lg:mx-[10px] rounded-[8px] p-[20px] lg:py-[50px]"
         >
-          <div className="flex flex-col gap-[20px] py-[80px] px-[60px] mx-[10px] w-2/3">
-            <span className="text-[4rem]/[4rem] font-bold">
+          <div className="flex flex-col gap-[20px] lg:py-[80px] lg:px-[60px] mx-[10px] lg:w-2/3">
+            <span className="text-[2rem]/[2rem] lg:text-[4rem]/[4rem] font-bold">
               Loans / Agriculture
             </span>
-            <span className="text-[1rem]/[2rem]">
+            <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[2rem]">
               These loans are meant to boost agricultural productivity by
               providing sufficient funds that farmers need to finance their
               activities efficiently. Farmers can use the funds for multiple
@@ -32,20 +32,20 @@ export default function LoansAgriculture() {
               disaster or pandemic.
             </span>
           </div>
-          <div className="flex items-center justify-center w-1/3">
+          <div className="flex items-center justify-center lg:w-1/3">
             <BarnIcon className="w-[60%] h-auto" />
           </div>
         </section>
         <section id="description" className="px-[15px] text-white">
-          <div className="flex items-center justify-center px-[80px] py-[60px] gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
-            <div className="flex items-center justify-center w-2/5">
-              <LeafIcon className="w-[50%] h-auto" />
+          <div className="flex flex-col lg:flex-row items-center justify-center p-[20px] lg:px-[80px] lg:py-[60px] gap-[20px] lg:gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex items-center justify-center lg:w-2/5">
+              <LeafIcon className="w-[10rem] lg:w-[60%] h-auto" />
             </div>
-            <div className="flex flex-col items-start gap-[20px] w-3/5">
-              <span className="text-white font-bold text-[4rem]/[4rem]">
+            <div className="flex flex-col items-start gap-[20px] lg:w-3/5">
+              <span className="text-white font-bold text-[1.5rem]/[1.5rem] lg:text-[4rem]/[4rem]">
                 Agricultural Loans
               </span>
-              <span className="text-white text-[0.8rem]/[2.4rem]">
+              <span className="text-white text-[0.8rem]/[2.4rem] lg:text-[1rem]/[2rem]">
                 Farmers may borrow from Php100K up payable within five (5)
                 years. Interest rate starts at 12% per annum on a diminishing
                 balance. Co-makers are not necessary, but clients will have to
@@ -57,20 +57,20 @@ export default function LoansAgriculture() {
             </div>
           </div>
         </section>
-        <section className="flex mx-[60px] justify-between text-[#396131]">
-          <div className="flex flex-col gap-[50px] w-3/5">
-            <span className="text-[2.5rem]/[3rem] font-semibold">
+        <section className="flex flex-col-reverse lg:flex-row mx-[20px] gap-[20px] lg:mx-[60px] justify-between text-[#396131]">
+          <div className="flex flex-col gap-[20px] lg:gap-[50px] lg:w-3/5">
+            <span className="text-[1.5rem]/[2.25rem] lg:text-[2.5rem]/[3rem] font-semibold">
               AGRICULTURAL LOANS SURPASS PROJECTED GROWTH
             </span>
-            <span className="text-[2rem]/[2rem] font-semibold">
+            <span className="text-[1rem]/[1rem] lg:text-[2rem]/[2rem] font-semibold">
               Muabar Carba, Product Manager
             </span>
             <div className="flex flex-col gap-[50px]">
               <div className="flex flex-col gap-[10px]">
-                <span className="text-[1.5rem]/[1.5rem] font-semibold">
+                <span className="text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-semibold">
                   Portfolio Grows to PHP 204.89M
                 </span>
-                <span className="text-[1rem]/[3rem]">
+                <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem]">
                   With strategic marketing, persistent effort, and brilliant
                   performance, this product achieved an unprecedented growth of
                   Php204.89M in 2023 or 24.46M higher than Php180.43M growth in
@@ -78,10 +78,10 @@ export default function LoansAgriculture() {
                 </span>
               </div>
               <div className="flex flex-col gap-[10px]">
-                <span className="text-[1.5rem]/[1.5rem] font-semibold">
+                <span className="text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-semibold">
                   Agricultural Loans
                 </span>
-                <span className="text-[1rem]/[3rem]">
+                <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem]">
                   Agricultural loans, including agrarian reform loans, are
                   secured lending products that provide financial assistance to
                   farmers engaged in agrarian reform or agricultural activities.
@@ -92,7 +92,7 @@ export default function LoansAgriculture() {
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center w-2/5">
+          <div className="flex items-center justify-center lg:w-2/5">
             <img
               src={img1}
               alt=""
@@ -102,12 +102,12 @@ export default function LoansAgriculture() {
         </section>
         <section
           id="kinds"
-          className="flex flex-col gap-[40px] p-[50px] bg-[#396131] rounded-[8px] mx-[15px] drop-shadow-lg text-white"
+          className="flex flex-col gap-[20px] lg:gap-[40px] p-[30px] lg:p-[50px] bg-[#396131] rounded-[8px] mx-[15px] drop-shadow-lg text-white"
         >
-          <span className="text-[2rem]/[2rem] text-center font-bold">
+          <span className="text-[1.5rem]/[1.5rem] lg:text-[2rem]/[2rem] text-center font-bold">
             Kinds of Agricultural Loans
           </span>
-          <div className="grid grid-cols-3 gap-x-[100px] text-center">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-[100px] gap-y-[30px] text-center">
             {[
               {
                 icon: <ShieldCheckIcon size={150} />,
@@ -131,52 +131,55 @@ export default function LoansAgriculture() {
               <div className="flex flex-col gap-[20px]" key={index}>
                 <div className="flex flex-col items-center gap-[20px]">
                   {kind.icon}
-                  <span className="text-[1.5rem]/[1.5rem] font-bold">
+                  <span className="text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-bold">
                     {kind.header}
                   </span>
                 </div>
-                <span className="text-[1rem]/[3rem] ">{kind.description}</span>
+                <span className="text-[0.8rem]/[2.4rem] lg:text-[1rem]/[3rem] ">
+                  {kind.description}
+                </span>
               </div>
             ))}
           </div>
         </section>
         <section
           id="story"
-          className="flex justify-between mx-[60px] text-[#396131]"
+          className="flex flex-col-reverse lg:flex-row gap-[20px] justify-between mx-[20px] lg:mx-[60px] text-[#396131]"
         >
-          <div className="flex flex-col gap-[40px] w-3/5">
+          <div className="flex flex-col gap-[20px] lg:gap-[40px] lg:w-3/5">
             <div className="flex flex-col">
-              <span className="text-[2rem]/[2rem] font-bold">
+              <span className="text-[1rem]/[2srem] lg:text-[2rem]/[2rem] font-bold">
                 Fall 7 Times, Stand 8 Times
               </span>
-              <span className="text-[1.5rem]/[1.5rem]">
+              <span className="text-[0.8rem]/[1.6rem] lg:text-[1.5rem]/[1.5rem]">
                 The Caneros Success Story
               </span>
             </div>
-            <div className="flex flex-col gap-[20px]">
+            <div className="flex flex-col gap-[10px] lg:gap-[20px]">
               {[
                 "The Caneros is a humble family from Digos, Davao del Sur whose source of living is their banana plantation. They were able to live a normal and happy life, send their kids to school, and expand their plantation as well. Then came their nightmare in 2018- the Panama disease, a destructive disease of bananas caused by the soil-inhabiting fungus species.",
                 "Suffering from devastating losses, the Caneros were forced to sell their properties. They were in the brink of hopelessness seeing their future crumble before their eyes.",
                 "With the remaining land that they have, their faith in God, they did their best to recover from the tragedy. It was during this time that 1VB Digos introduce them our Agri Secured Loan with scheme tailor-fitted for banana growers. Through this product, the Caneros obtained the funds they need to rehabilitate their plantation. They were able to expand their plantation from 1.5 has to 7 has.",
                 "'Dako jud kaayo natabang ang 1st Valley Bank samoa kay tungod sa ila pagpautang samoa nakabakod mi balik.' (1st Valley Bank helped us a lot that we were able to rise again.), said Mrs. Virginia Cameros Tobiano.",
               ].map((paragraph, index) => (
-                <span className="text-[1rem]/[3rem]" key={index}>
+                <span
+                  className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem]"
+                  key={index}
+                >
                   {paragraph}
                 </span>
               ))}
             </div>
           </div>
-          <div className="flex items-center justify-center w-2/5">
+          <div className="flex items-center justify-center lg:w-2/5">
             <img
               src={img2}
               alt=""
-              className="object-cover rounded-[12px] w-4/5 h-full"
+              className="object-cover rounded-[12px] lg:w-4/5 h-full"
             />
           </div>
         </section>
       </main>
-      <main className="hidden sm:block lg:hidden">Tablet</main>
-      <main className="block sm:hidden">Mobile</main>
     </>
   );
 }
--- src/pages/LoansSME.jsx
+++ src/pages/LoansSME.jsx
@@ -49,15 +49,17 @@ export default function LoansSME() {
 
   return (
     <>
-      <main className="hidden lg:flex flex-col gap-[80px] pb-[50px]">
+      <main className="flex flex-col gap-[80px] pb-[50px]">
         <section
           id="main"
           data-scroll
-          className="flex text-[#396131] bg-white drop-shadow-lg mx-[10px] rounded-[8px] py-[50px]"
+          className="flex flex-col-reverse lg:flex-row text-[#396131] bg-white drop-shadow-lg mx-[10px] rounded-[8px] p-[30px] lg:py-[50px]"
         >
-          <div className="flex flex-col gap-[20px] py-[80px] px-[60px] mx-[10px] w-2/3">
-            <span className="text-[4rem]/[4rem] font-bold">Loans / SME</span>
-            <span className="text-[1rem]/[2rem]">
+          <div className="flex flex-col gap-[20px] lg:py-[80px] lg:px-[60px] mx-[10px] lg:w-2/3">
+            <span className="text-[2rem] lg:text-[4rem]/[4rem] font-bold">
+              Loans / SME
+            </span>
+            <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[2rem]">
               Loan facility that funds small and medium enterprises engaged in
               legitimate wholesale or retail businesses. Funds can be used for
               (a) financing business expansion, (b) purchasing new heavy
@@ -65,20 +67,20 @@ export default function LoansSME() {
               (d) trade check discounting.
             </span>
           </div>
-          <div className="flex items-center justify-center w-1/3">
+          <div className="flex items-center justify-center lg:w-1/3">
             <TractorIcon className="w-[60%] h-auto" />
           </div>
         </section>
         <section id="description" className="px-[15px] text-white">
-          <div className="flex items-center justify-center px-[80px] py-[60px] gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
-            <div className="flex items-center justify-center w-2/5">
+          <div className="flex flex-col lg:flex-row items-center justify-center p-[20px] lg:px-[80px] lg:py-[60px] gap0-[20px] lg:gap-[80px] bg-[#396131] rounded-[8px] drop-shadow-lg">
+            <div className="flex items-center justify-center w-full lg:w-2/5">
               <FarmIcon className="w-[50%] h-auto" />
             </div>
-            <div className="flex flex-col items-start gap-[20px] w-3/5">
-              <span className="text-white font-bold text-[4rem]/[4rem]">
+            <div className="flex flex-col items-start gap-[20px] lg:w-3/5">
+              <span className="text-white font-bold text-[1.5rem]/[1.5rem] lg:text-[4rem]/[4rem]">
                 SME Loans
               </span>
-              <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col gap-[10px] lg:gap-[20px]">
                 {[
                   "Borrowers may choose between secured and unsecured SME loans. With secured SME, the borrower has to submit a collateral, usually land title, to secure the loan. The advantage of this type of loan is that borrowers enjoy lower interest rates.",
                   "Another option is the unsecured SME loan where collateral is not required but only hypothecation of assets as may be necessary.",
@@ -97,20 +99,20 @@ export default function LoansSME() {
         </section>
         <section
           id="description-2"
-          className="flex justify-between mx-[60px] text-[#396131]"
+          className="flex flex-col-reverse lg:flex-row justify-between mx-[20px] gap-[20px] lg:mx-[60px] text-[#396131]"
         >
-          <div className="flex flex-col gap-[50px] w-2/3">
-            <span className="text-[2.5rem]/[2.5rem] font-bold">
+          <div className="flex flex-col gap-[20px] lg:gap-[50px] lg:w-2/3">
+            <span className="text-[1.5rem]/[1.5rem] lg:text-[2.5rem]/[2.5rem] font-bold">
               SME BRINGS YOUR BUSINESS TO SUCCESS!
             </span>
-            <span className="text-[2rem]/[2rem] font-semibold">
+            <span className="text-[1rem]/[1rem] lg:text-[2rem]/[2rem] font-semibold">
               Bernard C. Paderes, Product Manager
             </span>
             <div className="flex flex-col gap-[20px]">
-              <span className="text-[1.5rem]/[1.5rem] font-semibold">
+              <span className="text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-semibold">
                 SMALL TO MEDIUM IN SIZE, BIG IN RESULTS
               </span>
-              <span className="text-[1rem]/[3rem]">
+              <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem]">
                 In spite of the many challenges, 1VB SME product manages to be
                 the force to reckon with. In 2023, SME grew to a Php862.24M
                 portfolio or 24.23% against 2022. Certainly, this product
@@ -119,7 +121,7 @@ export default function LoansSME() {
               </span>
             </div>
           </div>
-          <div className="flex justify-center w-1/3">
+          <div className="flex justify-center lg:w-1/3">
             <img
               src={img1}
               alt=""
@@ -129,21 +131,21 @@ export default function LoansSME() {
         </section>
         <section
           id="kinds"
-          className="flex flex-col p-[50px] gap-[40px] bg-[#396131] text-white mx-[15px] rounded-[10px] drop-shadow-lg"
+          className="flex flex-col p-[20px] lg:p-[50px] gap-[40px] bg-[#396131] text-white mx-[15px] rounded-[10px] drop-shadow-lg"
         >
-          <span className="text-center font-bold text-[2rem]/[2rem]">
+          <span className="text-center font-bold text-[1.5rem]/[2rem] lg:text-[2rem]/[2rem]">
             Kinds of SME Loans
           </span>
-          <div className="grid grid-cols-3 gap-x-[100px] gap-y-[90px]">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-[20px] lg:gap-x-[100px] lg:gap-y-[90px]">
             {smallBusinessLoans.map((loan, index) => (
               <div className="flex flex-col justify-between gap-[40px]">
                 <div className="flex flex-col items-center gap-[30px]">
                   <div className="flex">{loan.logo}</div>
                   <div className="flex flex-col gap-[20px]">
-                    <span className="text-center text-[1.5rem]/[1.5rem] font-bold">
+                    <span className="text-center text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-bold">
                       {loan.type}
                     </span>
-                    <span className="text-[1rem]/[3rem]">
+                    <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem]">
                       {loan.description}
                     </span>
                   </div>
@@ -154,7 +156,7 @@ export default function LoansSME() {
         </section>
         <section
           id="feats"
-          className="flex gap-[80px] justify-center text-[#396131] mx-[50px]"
+          className="flex flex-col lg:flex-row gap-[80px] justify-center text-[#396131] mx-[20px] lg:mx-[50px]"
         >
           {[
             {
@@ -176,22 +178,23 @@ export default function LoansSME() {
                 "The product enjoys an 11-year streak in portfolio increase",
             },
           ].map((feat, index) => (
-            <div className="flex flex-col gap-[20px] w-1/3" key={index}>
+            <div
+              className="flex flex-col gap-[20px] w-full lg:w-1/3"
+              key={index}
+            >
               <div className="flex flex-col justify-center items-center gap-[20px]">
                 {feat.icon}
-                <span className="text-[1.5rem]/[1.5rem] font-bold">
+                <span className="text-[1rem]/[1rem] lg:text-[1.5rem]/[1.5rem] font-bold">
                   {feat.header}
                 </span>
               </div>
-              <span className="text-[1rem]/[3rem] text-center">
+              <span className="text-[0.8rem]/[1.6rem] lg:text-[1rem]/[3rem] text-center">
                 {feat.description}
               </span>
             </div>
           ))}
         </section>
       </main>
-      <main className="hidden sm:block lg:hidden">Tablet</main>
-      <main className="block sm:hidden">Mobile</main>
     </>
   );
 }
